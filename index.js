32942880874efbe3529149b6da4b51efbb0f6783
Jamarion HInes <94207162+Jayspie@users.noreply.github.com>
2025-01-12 17:25:27 -0600

    Completed admin schedule
    GET: Retrieve schedule details from the schedule table.
    
    POST: Add a new task
    
    UPDATE: update the table by ID when work_date, last_name, first_name,location_name,location_address,start_time, or end_time changes.
    
    DELETE: Remove a task from the schedule table.

--- index.js
+++ index.js
@@ -245,6 +245,184 @@ app.delete("/location:id", async (req, res) => {
     }
 });
 
+//ADMIN FOR SCHEDULE
+app.get("/schedule:id", async (req, res) => { 
+    const admin = await sql`SELECT admin FROM employees WHERE id=${Number(
+        req.params.id.replace(":", "")
+    )}`;
+    if (admin[0]?.admin == true) {
+        try {
+            const schedule = await sql`
+            select *
+            from schedule
+            ORDER BY work_date ASC
+        `;
+            res.status(200).json(schedule);
+        } catch (error) {
+            console.error("[server] Error fetching employees:", error);
+            res.status(500).json({ error: "Failed to fetch employees" });
+        }
+    } else {
+        res.status(500).json({ error: "dont have acess" });
+    }
+});
+
+app.post("/schedule:id", async (req, res) => {
+    const admin = await sql`SELECT admin FROM employees WHERE id=${Number(
+        req.params.id.replace(":", "")
+    )}`;
+    if (admin[0]?.admin == true) {
+        const { work_date ,
+            last_name ,
+            first_name ,
+            location_name ,
+            location_address ,
+            start_time ,
+            end_time} = req.body;
+        try {
+            const result = await sql`
+      insert into schedule (work_date ,
+            last_name ,
+            first_name ,
+            location_name ,
+            location_address ,
+            start_time ,
+            end_time)
+      values (${work_date}, ${last_name}, ${first_name}, ${location_name},${location_address}, ${start_time}, ${end_time})
+      returning *
+    `;
+            res.status(201).json(result);
+        } catch (error) {
+            console.error("[server] Error adding employee:", error);
+            res.status(500).json({ error: "Failed to add employee" });
+        }
+    } else {
+        res.status(500).json({ error: "Dont have access" });
+    }
+});
+
+app.put("/schedule:id", async (req, res) => {
+    const admin = await sql`SELECT admin FROM employees WHERE id=${Number(
+        req.params.id.replace(":", "")
+    )}`;
+    if (admin[0]?.admin == true) {
+        const { id, work_date ,
+            last_name ,
+            first_name ,
+            location_name ,
+            location_address ,
+            start_time ,
+            end_time } = req.body;
+
+        try {
+            // Fetch the current values for the given employee.
+            const [schedule] = await sql`
+            SELECT work_date ,last_name ,first_name ,location_name ,location_address ,start_time ,end_time
+            FROM schedule 
+            WHERE id = ${id}`;
+
+            if (!schedule) {
+                return res.status(404).json({ error: "Employee not found" });
+            }
+
+            // Initialize an array to store update queries.
+            const updates = [];
+
+            if (work_date !== schedule.work_date) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET work_date = ${work_date} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (last_name !== schedule.last_name) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET last_name = ${last_name} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (first_name !== schedule.first_name) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET first_name = ${first_name} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (location_name !== schedule.location_name) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET location_name = ${location_name} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (location_address !== schedule.last_nalocation_addressme) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET location_address = ${location_address} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (start_time !== schedule.start_time) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET start_time = ${start_time} 
+            WHERE id = ${id}
+        `);
+            }
+
+            if (end_time !== schedule.end_time) {
+                updates.push(await sql`
+            UPDATE schedule 
+            SET end_time = ${end_time} 
+            WHERE id = ${id}
+        `);
+            }
+
+            // Execute all updates if there are changes.
+            if (updates.length > 0) {
+                const results = await Promise.all(updates);
+                return res.status(200).json({ message: "Employee updated", results });
+            } else {
+                return res.status(200).json({ message: "No changes made" });
+            }
+        } catch (error) {
+            console.error("[server] Error updating employee:", error);
+            res.status(500).json({ error: "Failed to update employee" });
+        }
+
+
+    } else {
+        res.status(500).json({ error: "Dont have access" });
+    }
+});
+
+app.delete("/schedule:id", async (req, res) => {
+    const admin = await sql`SELECT admin FROM employees WHERE id=${Number(
+        req.params.id.replace(":", "")
+    )}`;
+    if (admin[0]?.admin == true) {
+        try {
+            const schedule = await sql`
+            DELETE FROM schedule
+            WHERE id = ${req.body.id};
+        `;
+            res.status(200).json(schedule);
+        } catch (error) {
+            console.error("[server] Error fetching employees:", error);
+            res.status(500).json({ error: "Failed to fetch employees" });
+        }
+    } else {
+        res.status(500).json({ error: "dont have acess" });
+    }
+});
+
+
 
 
 
--- lsptimesheet.session.sql
+++ lsptimesheet.session.sql
@@ -16,6 +16,18 @@ CREATE TABLE Locations(
     long_2 FLOAT NOT NULL
 );
 
+--@BLOCK 
+CREATE TABLE SCHEDULE(
+    id SERIAL NOT NULL,
+    work_date DATE NOT NULL,
+    last_name varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    location_name TEXT NOT NULL,
+    location_address TEXT NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL
+)
+
 --@BLOCK
 ALTER TABLE Employees
 RENAME COLUMN roles to admin;
@@ -28,6 +40,9 @@ SELECT* FROM employees ORDER BY id ASC;
 
 --@BLOCK
 SELECT* FROM locations ORDER BY location_name ASC;
+
+--@BLOCK
+SELECT* FROM schedule ORDER BY id ASC;
 --@BLOCK
 INSERT INTO employees (id, last_name,first_name,admin)
 VALUES (2, 'hines','jay','false');
@@ -47,3 +62,8 @@ WHERE id = 6483;
 INSERT INTO locations (location_name, location_address, lat_1, long_1, lat_2, long_2) VALUES
 ('Central Park', 'New York, NY, USA', 40.785091, -73.968285, 40.784000, -73.970000),
 ('Eiffel Tower', 'Champ de Mars, Paris, France', 48.858844, 2.294351, 48.857500, 2.292000);
+
+--@BLOCK 
+INSERT INTO SCHEDULE (work_date ,last_name ,first_name ,location_name ,location_address ,start_time ,end_time) VALUES
+('2025-01-12', 'ass','bob', 'wall mart park','New York, NY,USA','14:30:00','22:30:00'),
+('2025-01-19', 'jay','hines', 'wall mart park','New York, NY,USA','14:30:00','22:30:00');
